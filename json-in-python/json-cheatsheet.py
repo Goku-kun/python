0dcd98ffa118713fc64b4061c1ef67e2dd214883
Goku-kun <jethvadharmarajsinh@gmail.com>
2020-11-27 19:14:01 +0530

    ADD: created a cheatsheet and incomplete but done till READ and WRITE to/from json

---
+++ json-in-python/json-cheatsheet.py
@@ -0,0 +1,38 @@
+import json
+# The json module will help us convert from and convert to JSON.
+
+# The equivalent datatypes of python in JSON can be described as follows:
+"""
+Python      --      JSON
+dict                object
+list                Array
+tuple               Array
+int, float, long    number
+True                true
+False               false
+str                 string
+None                null
+"""
+
+# python object to JSON conversion
+
+person = {"name":"Goku", "age": "immortal", "hasChildren":True, "forms": 6,"titles":["Saiyan", "Ultra Instinct", "Super Saiyan", "Martial Artist"]}
+
+personJSON = json.dumps(person, indent=4, sort_keys=True) # the dump command converts an object to a JSON object but dump s command will convert a string to a json object
+print(personJSON)
+
+# writing a JSON object ot a .json file
+with open("person.json", "w") as file:
+    json.dump(person, file, indent=2)
+    file.close()
+
+
+# JSON to python object conversion
+
+goku = json.loads(personJSON) # look at the hasChildren attribute, the True has come back to the python format.
+print(goku)
+
+# READING a .json file and making a python object(dictionary) out of it
+with open("person.json",'r') as file:
+    fileJSONread = json.load(file)
+print(fileJSONread)
